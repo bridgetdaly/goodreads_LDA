e48a20a646eb7f00afda473addbf7f864c73c327
Bridget Daly <bdaly223@gmail.com>
2021-05-16 12:08:00 -0500

    add data prep

---
+++ README.md
@@ -0,0 +1,3 @@
+# Book Review LDA
+
+Final project for Applied Bayesian Analysis (STATS271) Stanford Spring 2021. Dataset comes from the [UCSD Book Graph](https://sites.google.com/eng.ucsd.edu/ucsdbookgraph/home), originally scraped from [Goodreads](https://www.goodreads.com/) in 2017.
---
+++ data_prep.py
@@ -0,0 +1,61 @@
+import json
+import pandas as pd
+import numpy as np
+import fasttext
+import nltk
+
+def load_review_dat(path, genre):
+    
+    # keep 20% of reviews
+    reviews = []
+    with open(path) as f:
+        for line in f:
+            entry = json.loads(line)
+            if np.random.uniform() < 0.2:
+                reviews.append(entry)
+                
+    # convert to DF, retaining only relevant columns
+    reviews = pd.DataFrame(reviews, columns=['user_id','book_id','rating','review_text','n_votes','n_comments'])
+    reviews["n_likes"] = reviews["n_votes"] + reviews["n_comments"]
+    reviews = reviews.drop(["n_votes","n_comments"],axis=1)
+    reviews["genre"] = genre
+    
+    return(reviews)
+
+# stack data
+mystery = load_review_dat("data/goodreads_reviews_mystery.json", "mystery")
+history = load_review_dat("data/goodreads_reviews_history_biography.json", "history")
+fantasy = load_review_dat("data/goodreads_reviews_fantasy_paranormal.json", "fantasy")
+
+reviews = pandas.concat([mystery, history, fantasy], ignore_index=True)
+print("Reviews Loaded")
+print(reviews.group_by("genre").count()["user_id"])
+
+# filter to reviews in English
+language_model = fasttext.load_model('lid.176.bin')
+pred = language_model.predict(reviews["review_text"].str.replace('\n','').to_list())
+keep_ind = [i for i in range(len(pred[0])) if pred[0][i][0] == '__label__en' and pred[1][i][0] > .90]
+reviews = reviews.iloc[keep_ind,].reset_index(drop=True)
+print("Reviews Filtered to English")
+print(reviews.groupby("genre").count()["user_id"])
+
+# tokenize reviews
+reviews["tokenized_words"] = reviews["review_text"].apply(lambda review: [word.lower() for word in nltk.tokenize.word_tokenize(review) if word.isalpha()])
+
+# remove stop words
+stopwords = nltk.corpus.stopwords.words('english')
+reviews["tokenized_words"] = reviews["tokenized_words"].apply(lambda review: [word for word in review if word not in stopwords])
+print("Stop Words Removed")
+
+# lemmatization
+wnl = nltk.stem.wordnet.WordNetLemmatizer()
+reviews["tokenized_words"] = reviews["tokenized_words"].apply(lambda review: [wnl.lemmatize(word) for word in review])
+print("Lemmatized")
+
+# return to string to save
+reviews["tokenized_words"] = reviews["tokenized_words"].apply(lambda review: " ".join(review))
+reviews = reviews.drop(["review_text"],axis=1)
+
+# save
+reviews.to_csv("reviews.csv", index=False)
+print("Saved")
